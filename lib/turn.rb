cc2e1b2e661947576f2aac2b02ddaec42f46eaa8
Abijah Stokes <abijahstokes@gmail.com>
2018-09-23 12:22:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+
+def display_board(board)
+  puts "   |    |   "
+  puts "------------"
+  puts "   |    |   "
+  puts "------------"
+  puts "   |    |   "
+end
+
+def valid_move? (board, index)
+  !position_taken?(board, index) && index.between?(0,8)
+end
+
+def position_taken? (board, index)
+  if board [index] == " " || board [index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move (array, index, value = "X")
+  array[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:
+ X | X | X
+-----------
+   |   |
+-----------
+   |   |   "
+end
